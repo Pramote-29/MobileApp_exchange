17d05ce907c8aaee1e1a578201d21aaf3b8f426b
pramote <manaziza001@gmail.com>
2024-09-01 17:04:41 +0700

    exchange cur

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/screen/lastRate.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Exchange Currency",
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Exchange Currency"),
+      ),
+      body: const LatestRate(),
+    ),
+  ));
+}
